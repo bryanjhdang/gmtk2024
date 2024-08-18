33f33552ff9149bd51322d48399b132cbc685adf
jerrydngzh <jerrydeng67@gmail.com>
2024-08-17 21:19:29 -0700

    implement enemy base constructor and factory

--- scenes/game_manager.gd
+++ scenes/game_manager.gd
@@ -2,7 +2,10 @@ extends Node
 
 @onready var hud = %Hud
 
+
+# use score as metric of stage progression (i.e. enemy difficulty)
 var score: float
+var enemy = preload("res://characters/enemy/enemy_base.tscn")
 
 func _ready() -> void:
 	score = 100
@@ -16,3 +19,13 @@ func add_point(amount: float) -> void:
 func update_score_label() -> void:
 	if hud:
 		hud.update_score(score)
+
+# spawns (1) enemy per timer timeout
+func _on_timer_timeout():
+	var x = randf_range(20, 1000)
+	var y = randf_range(20, 1000)
+	var enemy_to_spawn = EnemyBase.new_enemy("", 1.0, 1.0, 100)
+	enemy_to_spawn.position = Vector2(x, y)
+	add_child(enemy_to_spawn)
+	
+	
