b991fb4cbb522ea7840f3940d7f102c2f229b190
jerrydngzh <jerrydeng67@gmail.com>
2024-08-18 17:13:58 -0700

    track how many spawned enemies on screen

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -1,7 +1,8 @@
 class_name Enemy
 extends Area2D
 
-@onready var game_manager = get_node("../../GameManager")
+@onready var game_manager: Node = get_node("../../GameManager")
+@onready var spawn_manager: Node2D = get_parent()
 const enemy = preload("res://characters/enemy/enemy.tscn")
 
 var type: String
@@ -27,5 +28,6 @@ func _on_body_entered(body):
 	if game_manager.score > value:
 		game_manager.add_point(value)
 		queue_free()
+		spawn_manager.num_enemies_spawned -= 1
 	else:
 		game_manager.set_game_over()
--- scenes/spawn_manager.gd
+++ scenes/spawn_manager.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
 var enemy = preload("res://characters/enemy/enemy.tscn")
+var num_enemies_spawned: int = 0
+
 
 func _on_timer_timeout():
+	var enemy_to_spawn = Enemy.new_enemy("", 1.0, 1.0, 100)
+	
+	# TODO spawn enemy within world boundaries
 	var x = randf_range(20, 1000)
 	var y = randf_range(20, 1000)
-	var enemy_to_spawn = Enemy.new_enemy("", 1.0, 1.0, 100)
 	enemy_to_spawn.position = Vector2(x, y)
 	add_child(enemy_to_spawn)
+	num_enemies_spawned += 1
