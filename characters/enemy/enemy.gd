ab79624f412fad9e31061574c922b7394750594a
jerrydngzh <zrd1@sfu.ca>
2024-08-18 19:39:05 -0700

    add mechanic to vary the sprite, value, scale of spawned fish

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -3,25 +3,14 @@ extends Area2D
 
 @onready var game_manager: Node = get_node("../../GameManager")
 @onready var spawn_manager: Node2D = get_parent()
-const enemy = preload("res://characters/enemy/enemy.tscn")
 
-var type: String
-var size_x: float
-var size_y: float
+const enemy = preload("res://characters/enemy/enemy.tscn")
 var value: float = 1
 
 # constructor for a enemy base object, passed in parameters modify the base object
 # to construct an upgraded enemy
-static func new_enemy(type: String, size_x: float, size_y: float, value: float) -> Enemy:
-	var new_enemy: Enemy = enemy.instantiate()
-	new_enemy.type = type
-	new_enemy.size_x = size_x
-	new_enemy.size_y = size_y
-	new_enemy.value = value
-	
-	# TODO: update the base enemy object based on passed in values above
-	
-	return new_enemy
+static func enemy_factory() -> Enemy:
+	return enemy.instantiate()
 
 # hanldes scoring mechanics for collided enemies
 func _on_body_entered(body):
--- scenes/spawn_manager.gd
+++ scenes/spawn_manager.gd
@@ -1,15 +1,36 @@
 extends Node2D
 
+# @onready var game_manager: Node = owner.get_node("GameManager")
+
 var enemy = preload("res://characters/enemy/enemy.tscn")
 var num_enemies_spawned: int = 0
+var enemy_types: Array = ["fish-enemy.png", "fish-cooked-svgrepo-com.svg", "puffer-fish.webp", "karp-fish.png"] 
+var enemy_size_values: Array = [1, 5, 20, 50, 500]
 
+
+func get_new_enemy(score: int = 100):		
+	var enemy = Enemy.enemy_factory()
+	
+	# adjust the fish's values	
+	#enemy.scale *= 1.2
+	
+	# TODO choose specific sprite depending on the type & size generated
+	var type = randi_range(0, 3)
+	var size = randi_range(0, 99) % 20
+	var sprite = load(str("res://assets/", enemy_types[type]))
+	
+	# TODO make the enemy's value correspond to the chose size	
+	enemy.value = 100 * randf_range(0, 0.5) + 100
+	enemy.get_child(0).texture = sprite
+	return enemy
 
 func _on_timer_timeout():
-	var enemy_to_spawn = Enemy.new_enemy("", 1.0, 1.0, 100)
+	var enemy_to_spawn = get_new_enemy()
 	
-	# TODO spawn enemy within world boundaries
+	# TODO spawn enemy within world boundaries of device dimension
 	var x = randf_range(20, 1000)
 	var y = randf_range(20, 1000)
+	
 	enemy_to_spawn.position = Vector2(x, y)
 	add_child(enemy_to_spawn)
 	num_enemies_spawned += 1
