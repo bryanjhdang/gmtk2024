8c15a92cf9a27f25b26ebc457095f2ffcce5ef18
Bryan <bryandang10@gmail.com>
2024-08-20 00:25:33 -0700

    new ability icon

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -1,16 +1,28 @@
 extends CanvasLayer
 
+@onready var game_manager: Node = %GameManager
+
 @onready var player = %Player
 @onready var frenzy_icon = $FrenzyIcon
 @onready var frenzyTimer = $FrenzyIcon/Timer
 @onready var dashTimer = $DashIcon/Timer
 @onready var dash_icon = $DashIcon
 
+@onready var regfish = $Fish1
+@onready var tadpole = $Fish2
+@onready var pufferfish = $Fish3
+@onready var anglerfish = $Fish4
+
+const pufferfish_value: float = 150.0
+const anglerfish_value: float = 300.0
+
 var time = 0
 
 func _ready() -> void:
 	frenzyTimer.one_shot = true
 	dashTimer.one_shot = true
+	pufferfish.hide()
+	anglerfish.hide()
 
 func _frenzyCooldown() -> void:
 	frenzyTimer.start()
@@ -29,6 +41,11 @@ func _process(delta):
 	dash_icon.pb.value = 100 - (dashTimer.time_left * 100 / 2)
 	time += delta
 	$timerLabel.text = "%d" % time
+	
+	if game_manager.score > 150:
+		pufferfish.show()
+	if game_manager.score > 300:
+		anglerfish.show()
 
 func _on_dash_timer_timeout():
 	player.can_dash = true
